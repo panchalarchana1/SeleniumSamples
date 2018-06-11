df734cf7aafe9064100d22526ff402c8219f5ac7
panchalarchana1 <panchalarchana1@gmail.com>
2018-06-11 16:44:30 +0530

    Update DateCapture.java
    None

--- src/java/DateCapture/DateCapture.java
+++ src/java/DateCapture/DateCapture.java
@@ -2,9 +2,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-/*Author : Archana Panchal 
- * This is a simple sample programme to test Date picker Functionality
+import org.openqa.selenium.chrome.ChromeDriver
+
+/* This is a simple sample programme to test Date picker Functionality
  * As Now a days we have many sites with Date pipcker widget - its very 
  * hard to select particular date ( future and past both )using selenium 
  * web-driver. 
@@ -19,7 +19,8 @@ public class DateCapture {
 		JavascriptExecutor javaExc = (JavascriptExecutor)webDrv;	
 		String tagName = webElem.getTagName(); // capturing tagname of an element
 		
-		if(!webElem.getAttribute("id").trim().equals("")){	 // Checking whether the Date input field ( Element) has an ID or not
+		// Checking whether the Date input field ( Element) has an ID or not
+		if(!webElem.getAttribute("id").trim().equals("")){	 
 			try{
 				//Checking if the date input field is under span tag or not, if not then get Element Id and fill the date input field with particular date.
 				if((!tagName.trim().equalsIgnoreCase("span"))){	
@@ -30,8 +31,7 @@ public class DateCapture {
 					javaExc.executeScript("document.getElementById('"+webElem.getAttribute("Id")+"').innerHTML = '"+dateFrom+"';");
 				}
 			}catch(Exception e){
-				System.out.println("Throws Exception" + e.getCause()+ "\n"); // Catch the excception.
-				
+				System.out.println("Throws Exception" + e.getCause()+ "\n"); // Catch the excception.			
 				
 			}
 		}else{ // if date input field (Element) has an ID.
@@ -58,7 +58,7 @@ public class DateCapture {
 			System.setProperty("webdriver.chrome.driver","C:\\src\\chromedriver.exe");// path of webdriver..
 			WebDriver webDriver = new ChromeDriver();
 			DateCapture dc = new DateCapture();		
-		    webDriver.get("http://spicejet.com/");	// Site address which needs to be tested using selenium			
+		        webDriver.get("http://spicejet.com/");	// Site address which needs to be tested using selenium			
 			String date= "20/06";	// Date to be placed in Date picker text field 
 			String xPath = "//*[@id='ctl00_mainContent_view_date1']";
 			WebElement we = webDriver.findElement(By.xpath("//*[@id='ctl00_mainContent_view_date1']")); //find element using xpath of the Date input field							
