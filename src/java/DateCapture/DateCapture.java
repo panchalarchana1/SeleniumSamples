3f3b9d050926f073c74b35aac76009f8c4d5ad18
panchalarchana1 <panchalarchana1@gmail.com>
2018-06-11 16:41:10 +0530

    DateCapture v1
    Sample Programme to  Automate Date field functionality

--- src/java/DateCapture/DateCapture.java
+++ src/java/DateCapture/DateCapture.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 /*Author : Archana Panchal 
  * This is a simple sample programme to test Date picker Functionality
- * Now a days we have many sites with Date pipcker widget - its very 
+ * As Now a days we have many sites with Date pipcker widget - its very 
  * hard to select particular date ( future and past both )using selenium 
  * web-driver. 
  * So I have just wrote a programe to put/fill the date field with date
@@ -15,19 +15,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DateCapture {
 	
-	public void pickDate(WebElement we1, String dt1, WebDriver wd,String xpath){ // Function Date pick
-		JavascriptExecutor js = (JavascriptExecutor)wd;	
-		String tagname = we1.getTagName(); // capturing tagname of an element
+	public void pickDate(WebElement webElem, String dateFrom, WebDriver webDrv,String xpath){ // Function Date pick
+		JavascriptExecutor javaExc = (JavascriptExecutor)webDrv;	
+		String tagName = webElem.getTagName(); // capturing tagname of an element
 		
-		if(!we1.getAttribute("id").trim().equals("")){	 // Checking whether the Date input field ( Element) has an ID or not
+		if(!webElem.getAttribute("id").trim().equals("")){	 // Checking whether the Date input field ( Element) has an ID or not
 			try{
-				//Checking if the date input field is under span tag or not if not then get Element Id and fill the date with particular date.
-				if((!tagname.trim().equalsIgnoreCase("span"))){	
-					js.executeScript("document.getElementById('"+we1.getAttribute("Id")+"').value = '"+dt1+"';");	
+				//Checking if the date input field is under span tag or not, if not then get Element Id and fill the date input field with particular date.
+				if((!tagName.trim().equalsIgnoreCase("span"))){	
+					javaExc.executeScript("document.getElementById('"+webElem.getAttribute("Id")+"').value = '"+dateFrom+"';");	
 				}
-				else if(tagname.trim().equalsIgnoreCase("span")){ 	// if the date input field is under Span tag			
-					we1.click();
-					js.executeScript("document.getElementById('"+we1.getAttribute("Id")+"').innerHTML = '"+dt1+"';");
+				else if(tagName.trim().equalsIgnoreCase("span")){ 	// if the date input field is under Span tag			
+					webElem.click();
+					javaExc.executeScript("document.getElementById('"+webElem.getAttribute("Id")+"').innerHTML = '"+dateFrom+"';");
 				}
 			}catch(Exception e){
 				System.out.println("Throws Exception" + e.getCause()+ "\n"); // Catch the excception.
@@ -37,12 +37,12 @@ public class DateCapture {
 		}else{ // if date input field (Element) has an ID.
 			try{		
 				//Checking if the date input field is span tag or not if not then get Element Id and fill the date with particular date.
-				if((!tagname.trim().equalsIgnoreCase("span"))){
-					js.executeScript("document.getElementsByClassName('"+we1.getAttribute("class")+"')[0].removeAttribute('readonly','readonly');");
-					js.executeScript("document.getElementsByClassName('"+we1.getAttribute("class")+"')[0].value= '"+dt1+"';");
-				}else if(tagname.trim().equalsIgnoreCase("span")){ // if the date input field is under Span tag
-						we1.click();
-						js.executeScript("document.getElementsByClassName('"+we1.getAttribute("class")+"')[0].innerHTML = '"+dt1+"';");
+				if((!tagName.trim().equalsIgnoreCase("span"))){
+					javaExc.executeScript("document.getElementsByClassName('"+webElem.getAttribute("class")+"')[0].removeAttribute('readonly','readonly');");
+					javaExc.executeScript("document.getElementsByClassName('"+webElem.getAttribute("class")+"')[0].value= '"+dateFrom+"';");
+				}else if(tagName.trim().equalsIgnoreCase("span")){ // if the date input field is under Span tag
+					webElem.click();
+					javaExc.executeScript("document.getElementsByClassName('"+webElem.getAttribute("class")+"')[0].innerHTML = '"+dateFrom+"';");
 				}	
 						
 				}					
@@ -54,10 +54,11 @@ public class DateCapture {
 	
   public static void main(String[] args) throws Exception {
 		try {
+			
 			System.setProperty("webdriver.chrome.driver","C:\\src\\chromedriver.exe");// path of webdriver..
 			WebDriver webDriver = new ChromeDriver();
 			DateCapture dc = new DateCapture();		
-		        webDriver.get("http://spicejet.com/");	// Site address which needs to be tested using selenium			
+		    webDriver.get("http://spicejet.com/");	// Site address which needs to be tested using selenium			
 			String date= "20/06";	// Date to be placed in Date picker text field 
 			String xPath = "//*[@id='ctl00_mainContent_view_date1']";
 			WebElement we = webDriver.findElement(By.xpath("//*[@id='ctl00_mainContent_view_date1']")); //find element using xpath of the Date input field							
@@ -72,3 +73,4 @@ public class DateCapture {
 	}
 
 }
+
